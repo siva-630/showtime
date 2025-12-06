cbfbdc2980452e6f9c3b01eb25fc56df568a7660
N.Siva Shankar <siva630029@gmail.com>
2025-12-06 22:02:00 +0530

    update

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -9,7 +9,7 @@ export const protectAdmin =async(req,res,next)=>{
 
     
     try{
-        const { userId }  = req.auth();
+        const { userId }  = req.auth;
 
         const user = await clerkClient .users.getUser(userId)
 
